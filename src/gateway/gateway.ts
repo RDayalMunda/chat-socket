d12404ba1dd4effaaf5ba36fbff05134615ed398
ramdayalm <ramdayal.m@gsl.in>
2025-04-03 10:53:02 +0530

    files linted

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,29 +1,34 @@
-import { OnModuleInit } from "@nestjs/common";
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import{ Server } from "socket.io"
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
 @WebSocketGateway({
   cors: {
-    origin: '*'
-  }
+    origin: '*',
+  },
 })
 export class MyGateway implements OnModuleInit {
-
   @WebSocketServer()
   server: Server;
 
   onModuleInit() {
-    this.server.on('connection', (socket)=>{
-      console.log('connected to:', socket.id)
+    this.server.on('connection', (socket) => {
+      console.log('connected to:', socket.id);
 
-      socket.on('disconnect', (reason)=>{
-        console.log('disconnected from:', socket.id)
-      })
-    })
+      socket.on('disconnect', () => {
+        console.log('disconnected from:', socket.id);
+      });
+    });
   }
 
-  @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any){
-    console.log('body', body)
-    this.server.emit('onMessage', {...body, from: 'server'})
+  @SubscribeMessage('message')
+  onNewMessage(@MessageBody() body: any) {
+    console.log('body', body);
+    this.server.emit('onMessage', { ...body, from: 'server' });
   }
 }
