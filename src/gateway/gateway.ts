8d4cf3cf9a541a37457613e144f6b60f7352dc3d
RDayalMunda <ramdayal.m@gsl.in>
2025-04-03 13:51:22 +0530

    typing changes

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -19,7 +19,6 @@ export class MyGateway implements OnModuleInit {
   onModuleInit() {
     this.server.on('connection', (socket) => {
       console.log('connected to:', socket.id);
-
       socket.on('disconnect', () => {
         console.log('disconnected from:', socket.id);
       });
@@ -29,6 +28,18 @@ export class MyGateway implements OnModuleInit {
   @SubscribeMessage('message')
   onNewMessage(@MessageBody() body: any) {
     console.log('body', body);
+    // check who sent message
+    // check the target room`
     this.server.emit('onMessage', { ...body, from: 'server' });
+  }
+
+  @SubscribeMessage('typing')
+  onTyping(@MessageBody() body: any) {
+    console.log('typing', body);
+    // check who is typing
+    // check the target rooms
+    // find out the users present in the target rooms
+    // emit to all the users present in the target rooms
+    this.server.emit('onTyping', { ...body, from: 'server' });
   }
 }
